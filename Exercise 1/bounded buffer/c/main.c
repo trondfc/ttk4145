1af5bb14ded2283e24c8dbf4f73c69e194a34a1b
Trond F. Christiansen <50290460+trondfc@users.noreply.github.com>
2024-01-10 08:18:50 +0100

    solved bound buffer c

--- Exercise 1/bounded buffer/c/main.c
+++ Exercise 1/bounded buffer/c/main.c
@@ -23,8 +23,8 @@ struct BoundedBuffer* buf_new(int size){
     
     pthread_mutex_init(&buf->mtx, NULL);
     // TODO: initialize semaphores
-    //sem_init(&buf->capacity,      0, /*starting value?*/);
-	//sem_init(&buf->numElements,   0, /*starting value?*/);
+    sem_init(&buf->capacity,      0, 5);
+	sem_init(&buf->numElements,   0, 0);
     
     return buf;    
 }
@@ -42,18 +42,27 @@ void buf_destroy(struct BoundedBuffer* buf){
 
 void buf_push(struct BoundedBuffer* buf, int val){    
     // TODO: wait for there to be room in the buffer
+    sem_wait(&buf->capacity);
+
     // TODO: make sure there is no concurrent access to the buffer internals
+    pthread_mutex_lock(&buf->mtx);
     
     rb_push(buf->buf, val);
     
-    
-    // TODO: signal that there are new elements in the buffer    
+    // TODO: signal that there are new elements in the buffer   
+    sem_post(&buf->numElements);
+    pthread_mutex_unlock(&buf->mtx); 
 }
 
 int buf_pop(struct BoundedBuffer* buf){
     // TODO: same, but different?
+    sem_wait(&buf->numElements);
+    pthread_mutex_lock(&buf->mtx);
     
-    int val = rb_pop(buf->buf);    
+    int val = rb_pop(buf->buf); 
+      
+    sem_post(&buf->capacity);
+    pthread_mutex_unlock(&buf->mtx); 
     
     return val;
 }
